0d3cc230e1d93c1fe1de55b3185a6bb42be8d264
dgmp88 <dgmprichard@gmail.com>
2017-03-17 13:23:02 +0000

    More fixes for osx

--- pywatch/pywatch.py
+++ pywatch/pywatch.py
@@ -6,15 +6,16 @@ import ignore
 import sys
 import os
 
-
 # Global variables
 last_modified = 0
+start_time = time.time()
+file_changed = ''
 restart_wait = 0.5 # in seconds
 
 # Simple class that reacts to changes
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        global last_modified
+        global last_modified, file_changed
         src_path = event.src_path
         if event.is_directory and sys.platform == 'darwin':
             # On OSX, we need to do this annoyingly
@@ -22,9 +23,15 @@ class MyHandler(FileSystemEventHandler):
 
         if src_path and not ignore.do_ignore(src_path):
             last_modified = time.time()
+            file_changed = src_path
+
 
     def check_event_files(self, event):
         files_in_dir = [event.src_path+"/"+f for f in os.listdir(event.src_path)]
+        files_in_dir = filter(lambda x: ignore.do_ignore(x) ==False, files_in_dir)
+        files_in_dir = filter(lambda x: os.path.getmtime(x) > start_time, files_in_dir)
+        files_in_dir = filter(lambda x: os.path.isdir(x) == False, files_in_dir)
+
         if len(files_in_dir) > 0:
             return max(files_in_dir, key=os.path.getmtime)
         else:
@@ -35,10 +42,9 @@ def restart(cmd):
     stop()
     time.sleep(0.1)
     start(cmd)
-    print('restarted')
+    print('%s changed, restarted' % (file_changed))
 
 def main():
-    print(sys.argv)
     if len(sys.argv) < 2:
         print('Usage: pywatch "runcmd"')
         raise Exception
